8b55faccc6d8ca19864753946e201f74a7004d3f
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2024-01-03 13:28:35 -0300

    Update .eslintrc.json and package.json
    Add pre-push.js and reports/generate-reports.js

--- reports/generate-reports.js
+++ reports/generate-reports.js
@@ -2,10 +2,15 @@ const { execSync } = require('child_process')
 const fs = require('fs')
 
 try {
-  if (fs.existsSync('reports/test-results.json'))
+  console.log('Generating reports...')
+  // remove old test-results.json
+  if (fs.existsSync('reports/test-results.json')) 
     fs.unlinkSync('reports/test-results.json')
+  // run tests and generate json report
   execSync('npm run test:json', { stdio: 'inherit' })
+  // wait until test-results.json is created
   while (!fs.existsSync('reports/test-results.json')) {}
+  
 } catch (error) {
   console.error('Tests failed. Please fix the tests before pushing.')
   process.exit(1)
