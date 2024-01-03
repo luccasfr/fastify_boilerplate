bd984aedd9350bd8e13e9a4e88464232899de9b8
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2024-01-03 16:20:36 -0300

    Add test summary report and update pre-commit script

--- pre-commit.js
+++ pre-commit.js
@@ -1,19 +1,40 @@
 const { execSync } = require('child_process')
+const fs = require('fs')
 
 let testPassing = true
 let testResults = ''
 
+function summarizeTest(testResultsJson) {
+  const data = JSON.parse(testResultsJson)
+  const numPassedTests = data.numPassedTests
+  const numFailedTests = data.numFailedTests
+  const numTotalTests = data.numTotalTests
+  const testSummary = JSON.stringify({
+    numPassedTests,
+    numFailedTests,
+    numTotalTests,
+    amountPassed: `${numPassedTests}/${numTotalTests}`,
+    pctPassed: (numPassedTests / numTotalTests) * 100,
+  })
+  fs.writeFileSync('reports/test-summary.json', testSummary)
+}
+
 try {
   testResults = execSync('npm run test', { stdio: 'inherit' })
+  const testResultsJson = fs.readFileSync('reports/test-results.json')
+  summarizeTest(testResultsJson)
 } catch (error) {
   testPassing = false
   console.error('Tests failed')
 }
 
 try {
-  execSync('git add reports/test-results.json reports/coverage-summary.json', {
-    stdio: 'inherit',
-  })
+  execSync(
+    'git add reports/test-results.json reports/coverage-summary.json reports/test-summary.json',
+    {
+      stdio: 'inherit',
+    },
+  )
 } catch (error) {
   console.error('Failed to add report files')
 } finally {
