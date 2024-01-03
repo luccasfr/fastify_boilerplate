90d3bb8138686b53f926d97377f9d43bbcf6615b
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2024-01-03 13:32:18 -0300

    Update test script and pre-push hook

--- pre-push.js
+++ pre-push.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process')
 
 try {
   console.log('Running tests before push...')
-  execSync('npm run test', { stdio: 'inherit' })
+  execSync('npm run prepush', { stdio: 'inherit' })
 } catch (error) {
   console.error('Tests failed. Please fix the tests before pushing.')
   process.exit(1)
