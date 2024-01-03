e12859a045c854fa149cdb23f2d9680432838ee6
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2024-01-03 11:10:41 -0300

    Update test command in package.json

--- pre-push.js
+++ pre-push.js
@@ -2,7 +2,7 @@ const { execSync } = require('child_process')
 
 try {
   console.log('Running tests before push...')
-  execSync('npm run test-and-commit', { stdio: 'inherit' })
+  execSync('npm run test', { stdio: 'inherit' })
 } catch (error) {
   console.error('Tests failed. Please fix the tests before pushing.')
   process.exit(1)
