f8c598866cb43c9e316c90ad81798da05340010a
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2024-03-08 15:23:50 -0300

    Update coverage summary and import statement in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { FastifyInstance } from 'fastify'
-import ExampleRouter from './exampleRouter'
 
 export default async function Routes(fastify: FastifyInstance) {
-  fastify.register(ExampleRouter, { prefix: '/example' })
+  fastify.register(import('./exampleRouter'), { prefix: '/example' })
 }
