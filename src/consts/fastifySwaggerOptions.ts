eef478a1a6826621b4397152d9887b1d7b7badc2
Lucas Ferreira <lucas.ferreira@jerasoft.inf.br>
2024-03-08 15:05:48 -0300

    Adding swagger JWT bearer option

--- src/consts/fastifySwaggerOptions.ts
+++ src/consts/fastifySwaggerOptions.ts
@@ -10,6 +10,22 @@ const fastifySwaggerOptions: FastifyRegisterOptions<SwaggerOptions> | undefined 
       version: '1.0.0',
     },
     servers: [],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+          description:
+            'JWT authentication. You must provide a valid JWT token in the header of your request.',
+        },
+      },
+    },
+    security: [
+      {
+        BearerAuth: [],
+      },
+    ],
   },
   transform: jsonSchemaTransform,
 }
